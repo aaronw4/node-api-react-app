57304f09daf4aa46a4b544f96fa5008bb3dab6c7
Aaron Williams <aaronrw4@yahoo.com>
2020-01-26 22:25:46 -0800

    Create useEffect and listed mapped projects in jsx

--- users/src/App.js
+++ users/src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    function fetchData() {
+      axios
+        .get('https://aw-node-api-challenge.herokuapp.com/projects')
+        .then(info => setProjects(info.data))
+        .catch(err => console.log(err))
+    }
+    fetchData();
+  },[]);
+
   return (
     <div className="App">
-      
+      {console.log(projects)}
+      <div className='title'>
+        <h1>Projects</h1>
+      </div>
+      <div className='projects'>
+        {projects.map(project => (
+          <div key={project.id}>
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
